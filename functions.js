94f29547b8f136f6c828483ef061a6c4adbbdb6b
FetullahOzkan <111522168+FetullahOzkan@users.noreply.github.com>
2022-12-06 17:05:59 +0100

    JS2 Week 04 Assignment01 has added

--- functions.js
+++ functions.js
@@ -1,10 +1,27 @@
-const getProductsOfNormalQuality = () => {};
+// This function returns products with normal quality.
 
-const getProductImageWithPngExtension = () => {};
+const getProductsOfNormalQuality = (pNormalQualityList) => {
+  return pNormalQualityList.filter((item) => item.quality == "Normal")}
 
-const getCalorieOfTheMostExpensiveProduct = () => {};
+// This function returns products whose image
 
-const sortByExpirationDate = () => {};
+  const getProductImageWithPngExtension=(pImage)=>{
+    return  pImage.filter((item)=> item.productImage.includes(".png"))
+}
+
+// This function returns the calorie of the
+
+const getCalorieOfTheMostExpensiveProduct = (pCalorie) => {
+         pCalorie.map((calorie) => calorie.price).sort((a, b) => b - a)[0];
+  return pCalorie.filter((calorie) => calorie.totalCalori[0]);
+};
+
+// This function sorts the products according to their expiration date from smallest to largest.
+
+const sortByExpirationDate= (pDate) => {
+        pDate.sort((a,b)=>{
+                return a.expireDate-b.expireDate
+            })} ;
 
 export {
   getProductsOfNormalQuality,
